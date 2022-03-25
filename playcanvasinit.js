20e2d57e0e05bb8328e145fe81fbf88367dfe633
quantumgames-studiosinc <98978887+quantumgames-studiosinc@users.noreply.github.com>
2022-03-25 14:37:57 -0600

    Update and rename blockcanvasplugin-main.js to playcanvasinit.js

--- playcanvasinit.js
+++ playcanvasinit.js
@@ -4,19 +4,26 @@ let yml = `
     color: ff0000 #color of the category
     icon: text.svg #icon of the category
     blocks:
-      - test: #there is a block that we call it test
+      - playcanvas init: #there is a block that we call it test
           shadows:
             - 1 #it has one shadow(Whenever no other value is present in the input, this value fills it)
           args:
             a: value
           function: test #the function that we call when this block is used. you can see definition of it in the next line
           code: >
-            func test(a, b) {
-              print(a)
-              b!()
-            }
+            var script = pc.createScript('script');
+
+      // initialize code called once per entity
+           script.prototype.initialize = function() {
+
+        };
+
+// update code called every frame
+        script.prototype.update = function(dt) {
+
+        };
           messages: #The messages we want to appear on the block (we put them before inputs)
-            - test
+            - playcanvas init
             -
           hasReturn: false #is it a block that returns something (ellipse ones)
           tooltip: test #the tooltip of the block
