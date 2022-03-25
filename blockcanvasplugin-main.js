244176206d80b2a65da785db114b2d3e61a68643
quantumgames-studiosinc <98978887+quantumgames-studiosinc@users.noreply.github.com>
2022-03-25 13:59:12 -0600

    Update blockcanvasplugin-main.js

--- blockcanvasplugin-main.js
+++ blockcanvasplugin-main.js
@@ -1,34 +1,24 @@
-console.log('BlockCanvas plugin has been implemented.')
 let yml = `
 ---
-  Initilize:
-    color: ff0000
-    icon: text.svg
+  Test: #name of the category
+    color: ff0000 #color of the category
+    icon: text.svg #icon of the category
     blocks:
-      - initilize:
-            shadows:
-              - 1
+      - test: #there is a block that we call it test
+          shadows:
+            - 1 #it has one shadow(Whenever no other value is present in the input, this value fills it)
           args:
-            a: statement
-          function: initilize
+            a: value
+          function: test #the function that we call when this block is used. you can see definition of it in the next line
           code: >
-var script = pc.createScript('script');
-
-// initialize code called once per entity
-script.prototype.initialize = function() {
-
-};
-
-// update code called every frame
-script.prototype.update = function(dt) {
-
-};
-
-
-          messages:
-            - script setup
+            func test(a, b) {
+              print(a)
+              b!()
+            }
+          messages: #The messages we want to appear on the block (we put them before inputs)
+            - test
             -
-          hasReturn: false
-          tooltip: test
+          hasReturn: false #is it a block that returns something (ellipse ones)
+          tooltip: test #the tooltip of the block
 `
 createBlocksFromYAML(yml)
