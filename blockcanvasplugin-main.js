eafc69707af8b4dcbffb66488401e990a1640a84
quantumgames-studiosinc <98978887+quantumgames-studiosinc@users.noreply.github.com>
2022-03-25 13:48:18 -0600

    Update and rename blockcanvas-plugin.js to blockcanvasplugin-main.js

---
+++ blockcanvasplugin-main.js
@@ -0,0 +1,34 @@
+console.log('BlockCanvas plugin has been implemented.')
+let yml = `
+---
+  Initilize:
+    color: ff0000
+    icon: text.svg
+    blocks:
+      - initilize:
+          shadows:
+            - 1
+          args:
+            a: statement
+          function: initilize
+          code: >
+var script = pc.createScript('script');
+
+// initialize code called once per entity
+script.prototype.initialize = function() {
+
+};
+
+// update code called every frame
+script.prototype.update = function(dt) {
+
+};
+
+
+          messages:
+            - script setup
+            -
+          hasReturn: false
+          tooltip: test
+`
+createBlocksFromYAML(yml)
