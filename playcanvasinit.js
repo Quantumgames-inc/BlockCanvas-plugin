80d9fa0174299cc8ba4d18fc3b09df414d9009b6
quantumgames-studiosinc <98978887+quantumgames-studiosinc@users.noreply.github.com>
2022-03-25 15:04:49 -0600

    Update playcanvasinit.js

--- playcanvasinit.js
+++ playcanvasinit.js
@@ -1,27 +1,26 @@
-console.log('hey! plugin js code executed')
 let yml = `
 ---
-  ver=1.0.0
-  Test:
-    color: ff0000
-    icon: text.svg
+ver=0.0.1
+  Test: #name of the category
+    color: ff0000 #color of the category
+    icon: text.svg #icon of the category
     blocks:
-      - test:
+      - test: #there is a block that we call it test
           shadows:
-            - 1
+            - 1 #it has one shadow(Whenever no other value is present in the input, this value fills it)
           args:
             a: value
-            b: statement
-          function: test
+            b: statement #we have two inputs one of them gives a normal value and the other one gives a program, a statement(as a lambda function)
+          function: test #the function that we call when this block is used. you can see definition of it in the next line
           code: >
             func test(a, b) {
               print(a)
               b!()
             }
-          messages:
+          messages: #The messages we want to appear on the block (we put them before inputs)
             - test
             -
-          hasReturn: false
-          tooltip: test
+          hasReturn: false #is it a block that returns something (ellipse ones)
+          tooltip: test #the tooltip of the block
 `
 createBlocksFromYAML(yml)
